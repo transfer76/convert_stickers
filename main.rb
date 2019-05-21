0cb325bad6d413c8a45a91b43a86d1bdbfd52a1a
transfer76 <shina76@ukr.net>
2019-05-21 23:53:51 +0300

    Add function start/stop

--- main.rb
+++ main.rb
@@ -9,7 +9,9 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
     Thread.start(rqst) do |rqst|
       case rqst.text
       when '/start', '/start start'
-        bot.api.send_message()
+        bot.api.send_message(chat_id: rqst.chat.id, text: "Hi, #{rqst.from.first_name} \nSend me sticker to get PNG file")
+      when '/stop'
+        bot.api.send_message(chat_id: rqst.chat.id, text: "Bye, #{rqst.from.first_name}")
       end
     end
   end
