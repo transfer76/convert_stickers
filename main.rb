b7930aa164a67cd0262f63fa9cc1191661170287
transfer76 <shina76@ukr.net>
2019-05-22 09:47:46 +0300

    Add sticker path

--- main.rb
+++ main.rb
@@ -12,6 +12,16 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
         bot.api.send_message(chat_id: rqst.chat.id, text: "Hi, #{rqst.from.first_name} \nSend me sticker to get PNG file")
       when '/stop'
         bot.api.send_message(chat_id: rqst.chat.id, text: "Bye, #{rqst.from.first_name}")
+      else
+        sticker = rqst.sticker
+        if sticker
+          sticker_path = bot.api.get_file(file_id: sticker.file_id).file_path
+          f = open("https://api.telegram.org/file/bot#{TOKEN}/#{sticker_path}")
+          File.write('sticker.png', f.read)
+          f.close
+        else
+          bot.api.send_message(chat_id: rqst.chat.id, text: "That was not a sticker....try again!")
+        end
       end
     end
   end
